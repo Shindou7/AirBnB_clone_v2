00fe4deb496fd9fe56adfcf8fa10de0b452ff53c
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-12-07 18:07:01 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,18 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Script to set up web server for deployment
 
-apt-get update
-apt-get install -y nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+default_sites="/etc/nginx/sites-available/default"
+location=$(grep -Fn location $default_sites | head -1 | cut -d":" -f1)
+hbnb_static="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"
+echo "<h1>Hello World!</h1>" | sudo tee "/data/web_static/releases/test/index.html"
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -hR ubuntu:ubuntu "/data/"
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+sudo sed -i "${location}i ${hbnb_static}" "${default_sites}"
+sudo service nginx restart
