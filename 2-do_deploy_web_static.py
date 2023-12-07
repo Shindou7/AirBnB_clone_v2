f07a184ee108497e7bb9701a4d7ec5bb063baf42
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-12-07 17:12:09 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python3
-'''
-Deploy files to remote server using Fabric
+from datetime import datetime
+from fabric.api import *
+from os import path
 
-'''
-from fabric.api import env, put, run
-import os.path
+
 env.hosts = ['18.234.193.22', '34.227.90.136']
 
 
+def do_pack():
+    """Generates datetimes.
+    """
+
+    d = datetime.now()
+    now = d.strftime('%Y%m%d%H%M%S')
+
+    local("mkdir -p versions")
+    local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
+
+
 def do_deploy(archive_path):
-    '''Upload achive to web servers and
-    Distributes an .tgz archive through web servers
-    '''
-    if not os.path.isfile(archive_path):
-        return False
-    try:
-        filename = archive_path.split('/')[-1]
-        no_ext = filename.split('.')[0]
-        path_no_ext = '/data/web_static/releases/{}/'.format(no_ext)
-        symlink = '/data/web_static/current'
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(path_no_ext))
-        run('tar -xzf /tmp/{} -C {}'.format(filename, path_no_ext))
-        run('rm /tmp/{}'.format(filename))
-        run('mv {}web_static/* {}'.format(path_no_ext, path_no_ext))
-        run('rm -rf {}web_static'.format(path_no_ext))
-        run('rm -rf {}'.format(symlink))
-        run('ln -s {} {}'.format(path_no_ext, symlink))
+    """Upload achive to web servers"""
+
+    if path.exists(archive_path):
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
+
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_path))
+
         return True
-    except Exception:
-        return False
+
+    return False
