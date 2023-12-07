e5610d1bcc5378d0d6ad3aa77fa3ca55d012b46e
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-12-07 17:02:11 +0100

    Update 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+Deploy files to remote server using Fabric
+
+'''
+from fabric.api import env, put, run
+import os.path
+env.hosts = ['18.234.193.22', '34.227.90.136']
+
+
+def do_deploy(archive_path):
+    '''Upload achive to web servers and
+    Distributes an .tgz archive through web servers
+    '''
+    if not os.path.isfile(archive_path):
+        return False
+    try:
+        filename = archive_path.split('/')[-1]
+        no_ext = filename.split('.')[0]
+        path_no_ext = '/data/web_static/releases/{}/'.format(no_ext)
+        symlink = '/data/web_static/current'
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(path_no_ext))
+        run('tar -xzf /tmp/{} -C {}'.format(filename, path_no_ext))
+        run('rm /tmp/{}'.format(filename))
+        run('mv {}web_static/* {}'.format(path_no_ext, path_no_ext))
+        run('rm -rf {}web_static'.format(path_no_ext))
+        run('rm -rf {}'.format(symlink))
+        run('ln -s {} {}'.format(path_no_ext, symlink))
+        return True
+    except Exception:
+        return False
