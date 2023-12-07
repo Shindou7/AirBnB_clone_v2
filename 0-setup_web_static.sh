eeaa98655f528a81612af6b41635a07d5ba782f0
Shindou <98887570+Shindou7@users.noreply.github.com>
2023-12-07 18:16:58 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,13 +6,21 @@ sudo apt-get -y install nginx
 sudo service nginx start
 
 default_sites="/etc/nginx/sites-available/default"
-location=$(grep -Fn location $default_sites | head -1 | cut -d":" -f1)
+location=$(grep -Fn "location / {" $default_sites | cut -d":" -f1)
+
 hbnb_static="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
 sudo mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"
-echo "<h1>Hello World!</h1>" | sudo tee "/data/web_static/releases/test/index.html"
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Hello World!
+  </body>
+</html>" | sudo tee "/data/web_static/releases/test/index.html"
+
 sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
-sudo chown -hR ubuntu:ubuntu "/data/"
+sudo chown -R ubuntu:ubuntu "/data/"
 
 sudo sed -i "${location}i ${hbnb_static}" "${default_sites}"
 sudo service nginx restart
